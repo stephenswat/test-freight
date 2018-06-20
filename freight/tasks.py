468840d05eb3ecea6c89d4078a77683eb9004928
Stephen Swatman <stephenswat@gmail.com>
2018-06-20 22:39:14 +0200

    Huey can now retrieve contracts from ESI

--- freight/tasks.py
+++ freight/tasks.py
@@ -1,13 +1,18 @@
 import itertools
 import logging
+import datetime
+import pprint
 
 from django.conf import settings
+from django.db.models import Q
+from django.db import transaction
 
 from huey import crontab
 from huey.contrib.djhuey import db_periodic_task
 
-from eve_auth.models import Character
+from eve_auth.models import Character as AuthCharacter
 from eve_esi import ESI
+from freight.models import Contract, Character
 
 
 logger = logging.getLogger(__name__)
@@ -30,11 +35,17 @@ def get_corporation_contracts(corporation_id, client):
 
         return results
 
+def parse_swagger_datetime(sdt):
+    return datetime.datetime.strptime(
+        str(sdt),
+        r"%Y-%m-%dT%H:%M:%S+00:00"
+    )
+
 @db_periodic_task(crontab(minute='*/10'))
 def update_contracts():
     client = ESI.get_client()
 
-    for c in Character.objects.filter(scope_read_contracts=True):
+    for c in AuthCharacter.objects.filter(scope_read_contracts=True):
         allegiance = client.request(
             ESI['get_characters_character_id'](character_id=c.id)
         ).data
@@ -52,7 +63,46 @@ def update_contracts():
             logger.warning('Scraper %d revoked their key.', c.id)
             continue
 
-        print(contracts)
+        interesting = [
+            x for x in contracts
+            if x['type'] == 'courier'
+            and x['assignee_id'] == settings.ALLIANCE_ID
+        ]
+
+        with transaction.atomic():
+            for i in interesting:
+                issuer = Character.objects.get_from_db_or_esi(i['issuer_id'])
+
+                if i['acceptor_id'] != 0:
+                    acceptor = Character.objects.get_from_db_or_esi(i['acceptor_id'])
+                else:
+                    acceptor = None
+
+                Contract.objects.update_or_create(
+                    id=i['contract_id'],
+                    defaults={
+                        'title': i['title'],
+                        'volume': i['volume'],
+                        'status': Contract.STATUS_MAP[i['status']],
+                        'issuer': issuer,
+                        'acceptor': acceptor,
+                        'collateral': i['collateral'],
+                        'reward': i['reward'],
+                        'date_accepted': parse_swagger_datetime(i['date_accepted']) if 'date_accepted' in i else None,
+                        'date_completed': parse_swagger_datetime(i['date_completed']) if 'date_completed' in i else None,
+                        'date_expired': parse_swagger_datetime(i['date_expired']),
+                        'date_issued': parse_swagger_datetime(i['date_issued']),
+                        'days_to_complete': i['days_to_complete'],
+                        'start_location_id': i['start_location_id'],
+                        'end_location_id': i['end_location_id']
+                    }
+                )
+
+            Contract.objects.filter(
+                ~Q(status=Contract.STATUS_DISAPPEARED),
+                ~Q(id__in={x['contract_id'] for x in interesting})
+            ).update(status=Contract.STATUS_DISAPPEARED)
+
         return
     else:
         logger.error('No suitable scraper was available!')
