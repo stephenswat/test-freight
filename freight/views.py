b197d8069019e17cc30807d69f49f965b049df8b
Stephen Swatman <stephenswat@gmail.com>
2018-06-20 23:56:10 +0200

    A simple contract list now works

--- freight/views.py
+++ freight/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from django.views.generic.base import View, TemplateView
 
-from freight.models import Route
+from freight.models import Route, Contract
 
 
-class ContractListView(View):
-    def get(self, request):
-        return render(request, 'freight/contract_list.html')
+class ContractListView(TemplateView):
+    template_name = 'freight/contract_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['contracts'] = (
+            Contract.objects
+            .filter(status=Contract.STATUS_OUTSTANDING)
+        )
+        return context
 
 
 class CalculatorView(TemplateView):
