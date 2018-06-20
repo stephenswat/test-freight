c186af144f0e8ab3f9fcf2a9c6bfaf8a2a7e58b1
Stephen Swatman <stephenswat@gmail.com>
2018-06-20 23:40:18 +0200

    Add the freight calculator

--- freight/views.py
+++ freight/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.views.generic.base import View
+from django.views.generic.base import View, TemplateView
+
+from freight.models import Route
 
 
 class ContractListView(View):
@@ -7,6 +9,10 @@ class ContractListView(View):
         return render(request, 'freight/contract_list.html')
 
 
-class CalculatorView(View):
-    def get(self, request):
-        return render(request, 'index.html')
+class CalculatorView(TemplateView):
+    template_name = 'freight/calculator.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['routes'] = Route.objects.all()
+        return context
