e9a71d705473d6291130337ae7d9392d031fb174
Stephen Swatman <stephenswat@gmail.com>
2018-06-21 14:18:04 +0200

    Add a simple leaderboard

--- freight/urls.py
+++ freight/urls.py
@@ -9,5 +9,6 @@ app_name = 'freight'
 urlpatterns = [
     path('', freight.views.ContractListView.as_view(), name='contract_list'),
     path('calculator/', freight.views.CalculatorView.as_view(), name='calculator'),
+    path('leaderboard/', freight.views.LeaderboardView.as_view(), name='leaderboard'),
     path('view/<int:cid>/', freight.views.IngameContractView.as_view(), name='ingame_contract'),
 ]
--- freight/views.py
+++ freight/views.py
@@ -5,8 +5,9 @@ from django.views.generic.base import View, TemplateView
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
+from django.db.models import Count, Sum, Avg, Q, F, Subquery, OuterRef, ExpressionWrapper, fields
 
-from freight.models import Route, Contract
+from freight.models import Route, Contract, Character
 
 
 class ContractListView(TemplateView):
@@ -31,6 +32,48 @@ class CalculatorView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['routes'] = Route.objects.all()
         return context
+
+
+class LeaderboardView(View):
+    def get(self, request):
+        query = (
+            Character.objects
+            .filter(accepted_contracts__status=Contract.STATUS_FINISHED)
+            .annotate(contracts_completed=Count(
+                'accepted_contracts',
+            ))
+            .filter(contracts_completed__gt=10)
+            .annotate(volume_moved=Sum(
+                'accepted_contracts__volume',
+            ))
+            .annotate(average_ttc=Avg(
+                Subquery(
+                    Contract.objects
+                    .filter(
+                        status=Contract.STATUS_FINISHED,
+                        acceptor=OuterRef('id')
+                    )
+                    .annotate(
+                        ttc=ExpressionWrapper(
+                            F('date_completed') - F('date_accepted'),
+                            output_field=fields.DurationField()
+                        )
+                    )
+                    .values('ttc')
+                ),
+                output_field=fields.DurationField()
+            ))
+        )
+
+        return render(
+            request,
+            'freight/leaderboard.html',
+            context={
+                'ranking_completed': query.order_by('-contracts_completed')[:5],
+                'ranking_volume': query.order_by('-volume_moved')[:5],
+                'ranking_ttc': query.order_by('average_ttc')[:5],
+            }
+        )
 
 
 @method_decorator(csrf_exempt, name='dispatch')
