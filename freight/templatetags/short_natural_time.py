2a6128b3727ef918c3c4a2cc31999a1aa10d801e
Stephen Swatman <stephenswat@gmail.com>
2018-08-07 21:46:27 +0200

    Add the short natural time templatetag

---
+++ freight/templatetags/short_natural_time.py
@@ -0,0 +1,91 @@
+"""
+
+STOLEN FROM:
+
+https://github.com/ollieglass/django-shortnaturaltime
+
+"""
+
+
+from django import template
+from django.utils.timezone import utc
+
+import time
+from datetime import datetime, timedelta, date
+
+register = template.Library()
+
+def _now():
+    # return datetime.utcnow().replace(tzinfo=utc)
+    return datetime.now()
+
+def abs_timedelta(delta):
+    if delta.days < 0:
+        now = _now()
+        return now - (now + delta)
+    return delta
+
+def date_and_delta(value):
+    now = _now()
+    if isinstance(value, datetime):
+        date = value
+        delta = now - value
+    elif isinstance(value, timedelta):
+        date = now - value
+        delta = value
+    else:
+        try:
+            value = int(value)
+            delta = timedelta(seconds=value)
+            date = now - delta
+        except (ValueError, TypeError):
+            return (None, value)
+    return date, abs_timedelta(delta)
+
+def shortnaturaldelta(value):
+    now = _now()
+    date, delta = date_and_delta(value)
+
+    if date is None:
+        return value
+
+    days = abs(delta.days)
+    hours = (abs(delta.seconds) // (60 * 60)) % 24
+
+    minutes = (abs(delta.seconds) // (60)) % 60
+
+    if days >= 1:
+        return "%d day, %d hours" % (days, hours)
+    elif hours >= 1:
+        return "%d hours" % (hours)
+    else:
+        return "%d minutes" % (minutes)
+
+    if days < 1:
+        if seconds < 120:
+            return "1m"
+        elif 120 <= seconds < 3600:
+            return "%dm" % (seconds // 60)
+        elif 3600 <= seconds < 3600*2:
+            return "1h"
+        elif 3600 < seconds:
+            return "%dh" % (seconds // 3600)
+    else:
+        if days == 1:
+            return "1d"
+        else:
+            return "%dd" % days
+
+@register.filter
+def short_natural_time(value):
+    now = _now()
+    date, delta = date_and_delta(value)
+    if date is None:
+        return value
+
+    delta = shortnaturaldelta(delta)
+
+    if delta == "a moment":
+        return "now"
+
+    return delta
