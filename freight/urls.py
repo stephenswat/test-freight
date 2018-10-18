1d4ad7e95c9546ddec17820f57fc5039cf811249
Stephen Swatman <stephenswat@gmail.com>
2018-10-18 16:08:50 +0200

    Add a calc/ redirect to calculator/

--- freight/urls.py
+++ freight/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic.base import RedirectView
 
 import freight.views
 
@@ -9,6 +10,7 @@ app_name = 'freight'
 urlpatterns = [
     path('', freight.views.ContractListView.as_view(), name='contract_list'),
     path('calculator/', freight.views.CalculatorView.as_view(), name='calculator'),
+    path('calc/', RedirectView.as_view(url='/calculator/', permanent=True)),
     path('leaderboard/', freight.views.LeaderboardView.as_view(), name='leaderboard'),
     path('view/<int:cid>/', freight.views.IngameContractView.as_view(), name='ingame_contract'),
 ]
